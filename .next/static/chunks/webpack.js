2874b3983e65280fc2d131aefa60f8ddc442b66a
Christian <71664957+Christian-Mar@users.noreply.github.com>
2021-08-28 15:50:00 +0200

    Post detail page added

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "94c01b677b81e69fc494";
+/******/ 	var hotCurrentHash = "2d5753522ba78f611722";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
