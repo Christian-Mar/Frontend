2661bf624c0f1ccab4b3d31bea287bbc490e8c66
Christian <71664957+Christian-Mar@users.noreply.github.com>
2021-09-02 21:56:57 +0200

    Project finished

--- pages/_document.js
+++ pages/_document.js
@@ -7,7 +7,8 @@ class MyDocument extends Document {
 				<Head />
 				<body>
 					<Main />
-          <NextScript />
+					<NextScript />
+					<div id='notifications'> </div>
 				</body>
 			</Html>
 		);
