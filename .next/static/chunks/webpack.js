2468e5408b345edd8a805ca4b124df66e66de6d3
Christian <71664957+Christian-Mar@users.noreply.github.com>
2021-09-05 21:02:33 +0200

    Corrections done

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "4fd8e144a443f0126839";
+/******/ 	var hotCurrentHash = "91d1c475963abb239953";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- pages/contact.js
+++ pages/contact.js
@@ -3,8 +3,7 @@ import Head from 'next/head';
 import ContactForm from '../components/contact/contact-form';
 
 function ContactPage () {
-  return 
-  <Fragment>
+  return <Fragment>
   <Head>
     <title>Contacteer me</title>
     <meta name='description' content='Stuur een berichtje'/>
